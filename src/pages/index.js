008ef25b6f6a55863d6ef6b3d1221d69924db238
Renato Melo <renatoalves92@gmail.com>
2021-02-27 18:26:18 -0300

    fix(images): fix images src

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,24 @@
+import PropTypes from 'prop-types'
 import DefaultHome from './home/_index'
+import axiosInstance from '../services/axios';
 
-export default function Home() {
+export async function getServerSideProps(context) {
+  const axios = axiosInstance('backend')
+  const products = await axios.get('/').then(response => response.data.products)
+
+  return {
+    props: {
+      products
+    }
+  }
+}
+
+export default function Home({ products }) {
   return (
-    <DefaultHome />
+    <DefaultHome products={products} />
   )
+}
+
+Home.propTypes = {
+  products: PropTypes.array
 }
