ef7064e74759bf619f9130d93114e008f6f8444a
Renato <renatoalves92@gmail.com>
2021-05-09 21:26:44 -0300

    feat: create new pages to fit the app proposal

---
+++ src/pages/estudos/index.tsx
@@ -0,0 +1,8 @@
+export default function Estudos() {
+  return (
+    <>
+      <h1>Estudos</h1>
+      <p>Projetos e tecnologias que estou estudando no momento</p>
+    </>
+  )
+}
---
+++ src/pages/sobre-mim/index.tsx
@@ -0,0 +1,33 @@
+/* eslint-disable react/no-unescaped-entities */
+export default function AboutMe() {
+  return (
+    <main>
+      <figure>
+        <img src="/images/profile-pic.png" alt="Renato" />
+      </figure>
+      <div>
+        <h1>Sobre mim</h1>
+        <h2>Aspectos pessoais</h2>
+        <p>
+          What is the most resilient parasite? Bacteria? A virus? An intestinal worm? An idea.
+          Resilient... highly contagious, Cobb (Inception, 2010)
+        </p>
+        <br />
+        <hr />
+        <br />
+        <p>
+          Front-End Developer completely in love with the profession, I discovered that one of my
+          favorite hobbies - and I have many - is to study programming. Today, I consider myself a
+          web development evangelist. With daily study and participation in renowned projects such
+          as Carrefour, Peugeot/Citroen, Hering, SESC, government projects, among others, I keep
+          myself focused on the goal of becoming a full web developer, Javascript skilled. The full
+          web developer that I want to become can handle from conception to solution delivery, going
+          through User Experience and User Interface development, delivering front-end, back -end
+          and mobile, and ensuring the application deploy. Of course that I don't want to become a
+          specialist in all these stuffs, that's impossible, but having this background, although
+          difficult, is very possível. The road is long, but so pleasant. This is the way!
+        </p>
+      </div>
+    </main>
+  )
+}
